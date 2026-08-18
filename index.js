807d7635c3e8e502521b6519af48085b4736019c
alqadryhtan501-max <alqadryhtan501@gmail.com>
2026-08-19 01:15:48 +0300

    Fix module path for modalHandler import

--- index.js
+++ index.js
@@ -7,7 +7,29 @@ const {
 
 const setupPanel = require('./src/commands/setupPanel');
 const { handleButtons } = require('./src/commands/buttons/buttonHandler');
-const { handleModals } = require('./src/modals/modalHandler');
+const { handleModals } = require('./src/commands/src/modals/modalHandler');node:internal/modules/cjs/loader:1522
+  throw err;
+  ^
+
+Error: Cannot find module './src/modals/modalHandler'
+Require stack:
+- /data/data/com.termux/files/home/military-bot-/index.js
+    at Module._resolveFilename (node:internal/modules/cjs/loader:1519:15)
+    at wrapResolveFilename (node:internal/modules/cjs/loader:1073:27)
+    at defaultResolveImplForCJSLoading (node:internal/modules/cjs/loader:1097:10)
+    at resolveForCJSWithHooks (node:internal/modules/cjs/loader:1124:12)
+    at Module._load (node:internal/modules/cjs/loader:1296:5)
+    at wrapModuleLoad (node:internal/modules/cjs/loader:255:19)
+    at Module.require (node:internal/modules/cjs/loader:1619:12)
+    at require (node:internal/modules/helpers:191:16)
+    at Object.<anonymous> (/data/data/com.termux/files/home/military-bot-/index.js:10:26)
+    at Module._compile (node:internal/modules/cjs/loader:1873:14) {
+  code: 'MODULE_NOT_FOUND',
+  requireStack: [ '/data/data/com.termux/files/home/military-bot-/index.js' ]
+}
+
+Node.js v26.3.1
+~/military-bot- $
 
 const client = new Client({
     intents: [
