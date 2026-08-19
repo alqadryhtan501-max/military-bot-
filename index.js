5ee0982f7a017bfc90ac2b839b3c9c747b05ef23
alqadryhtan501-max <alqadryhtan501@gmail.com>
2026-08-19 23:17:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,53 @@
 require('dotenv').config();
 
 const {
-    Client,
-    GatewayIntentBits
+Client,
+GatewayIntentBits
 } = require('discord.js');
 
 const setupPanel = require('./src/commands/setupPanel');
 const { handleButtons } = require('./src/commands/buttons/buttonHandler');
 const { handleModals } = require('./src/commands/modals/modalHandler');
+
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
-    ]
+intents: [
+GatewayIntentBits.Guilds,
+GatewayIntentBits.GuildMessages,
+GatewayIntentBits.MessageContent
+]
 });
 
 client.once('ready', () => {
-    console.log(`🤖 تم تشغيل البوت: ${client.user.tag}`);
+console.log("🤖 تم تشغيل البوت: ${client.user.tag}");
 });
 
 client.on('interactionCreate', async (interaction) => {
+try {
 
-    try {
-
-        if (interaction.isChatInputCommand()) {
-
-            if (interaction.commandName === 'setup-panel') {
-                return await setupPanel.execute(interaction);
-            }
-
-        }
-
-        if (interaction.isButton()) {
-            return await handleButtons(interaction);
-        }
-
-        if (interaction.isModalSubmit()) {
-            return await handleModals(interaction);
-        }
-
-    } catch (error) {
-        console.error('Interaction Error:', error);
-
-        if (!interaction.replied && !interaction.deferred) {
-            await interaction.reply({
-                content: '❌ حدث خطأ أثناء تنفيذ الأمر.',
-                ephemeral: true
-            });
+    if (interaction.isChatInputCommand()) {
+        if (interaction.commandName === 'setup-panel') {
+            return await setupPanel.execute(interaction);
         }
     }
+
+    if (interaction.isButton()) {
+        return await handleButtons(interaction);
+    }
+
+    if (interaction.isModalSubmit()) {
+        return await handleModals(interaction);
+    }
+
+} catch (error) {
+    console.error('Interaction Error:', error);
+
+    if (!interaction.replied && !interaction.deferred) {
+        await interaction.reply({
+            content: '❌ حدث خطأ أثناء تنفيذ الأمر.',
+            ephemeral: true
+        });
+    }
+}
 
 });
 
